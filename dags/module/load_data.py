1be8e3d736474a22004503131ea87f7499bfe112
SuminHam <hsm4317@gmail.com>
2024-05-16 15:45:55 +0000

    feat - load_data 추가
    #9

---
+++ dags/module/load_data.py
@@ -0,0 +1,17 @@
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+
+import pandas as pd
+
+
+def load(**context):
+    hook = PostgresHook(postgres_conn_id="postgres_default")
+    conn = hook.get_conn()
+    stmt = """
+            SELECT *
+              FROM data.movies
+            """
+
+    data = pd.read_sql(stmt, conn)
+
+    # 데이터프레임을 딕셔너리로 변환하여 반환
+    return data.to_dict(orient="list")
