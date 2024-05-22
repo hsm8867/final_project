849a1320c95489c02d4b7e1c510ba7d05aa296f0
kimjae-1 <kimjae1@g.skku.edu>
2024-05-22 07:26:35 +0000

    feat: airflow 모델 버전(transit) 추가
     #15

--- dags/movie_dag.py
+++ dags/movie_dag.py
@@ -4,7 +4,8 @@ from airflow.operators.empty import EmptyOperator
 from pendulum import datetime
 
 from module import train
-
+from module.model_version import create_model_version
+from module.transit_model import transition_model_stage
 
 @dag(
     start_date=datetime(2024, 1, 1),
@@ -21,10 +22,21 @@ def movie_pipeline():
         python_callable=train.train_fn,
         op_kwargs={"experiment_name": "movie_model"},
     )
+    model_create_task = PythonOperator(
+        task_id="model_create_task",
+        python_callable=create_model_version,
+        op_kwargs={"model_name": "movie_model"},
+    )
+    model_transition_task = PythonOperator(
+        task_id="model_transition_task",
+        python_callable=transition_model_stage,
+        op_kwargs={"model_name": "movie_model"},
+    )
 
     end_task = EmptyOperator(task_id="end_task")
 
-    start_task >> train_task >> end_task
+    start_task >> [train_task >> model_create_task >> model_transition_task] >> end_task
 
 
 movie_pipeline()
+
