a46bf1fcd64277a3eb742845c33620e249d69ae7
SuminHam <hsm4317@gmail.com>
2024-05-26 09:52:46 +0000

    feat - FastAPI: redis 추가
    #20

--- app/routers/movie.py
+++ app/routers/movie.py
@@ -11,14 +11,24 @@ from app.models.schemas.movie_ import (
 )
 from app.models.db.movie_model import Movie
 
+from app.core.redis import redis_cache, key_builder
+from app.core.logger import logger
+
 router = APIRouter()
 
 
 @router.get("/list", response_model=BaseResponse[List[MovieResp]])
 async def movie_list_by_name(movie_name: str) -> BaseResponse[List[MovieResp]]:
-    async with AsyncScopedSession() as session:
-        stmt = select(Movie).where(Movie.movienm == movie_name)
-        results = (await session.execute(stmt)).scalars()
+    _key = key_builder("read_movie_list_by_name")
+
+    if await redis_cache.exists(_key):
+        logger.debug("Cache hit")
+        result = await redis_cache.get(_key)
+    else:
+        logger.debug("Cache miss")
+        async with AsyncScopedSession() as session:
+            stmt = select(Movie).where(Movie.movienm == movie_name)
+            results = (await session.execute(stmt)).scalars()
 
     return HttpResponse(
         content=[
@@ -38,9 +48,16 @@ async def movie_list_by_name(movie_name: str) -> BaseResponse[List[MovieResp]]:
 
 @router.get("/{movie_name}", response_model=BaseResponse)
 async def read_movie_by_name(movie_name: str) -> BaseResponse[MovieResp]:
-    async with AsyncScopedSession() as session:
-        stmt = select(Movie).where(Movie.movienm == movie_name)
-        result = (await session.execute(stmt)).scalar()
+    _key = key_builder("read_movie_by_name")
+
+    if await redis_cache.exists(_key):
+        logger.debug("Cache hit")
+        result = await redis_cache.get(_key)
+    else:
+        logger.debug("Cache miss")
+        async with AsyncScopedSession() as session:
+            stmt = select(Movie).where(Movie.movienm == movie_name)
+            result = (await session.execute(stmt)).scalar()
 
     return HttpResponse(
         content=MovieResp(
