4b495d26a13d6148bb9dbcafbf922f82099d1759
SuminHam <hsm4317@gmail.com>
2024-05-27 08:25:55 +0000

    feat - dependency 추가
    #20

--- app/core/errors/error.py
+++ app/core/errors/error.py
@@ -1,10 +1,14 @@
 ERROR_400_MOVIE_NOT_FOUND = "40000"
+ERROR_401_INVALID_API_KEY = "40100"
+
+"""
 ERROR_400_CLASS_NOTICE_NOT_FOUND = "40001"
 ERROR_400_CLASS_CREATION_FAILED = "40002"
 ERROR_400_CLASS_NOTICE_CREATION_FAILED = "40003"
 ERROR_400_CLASS_NOTICE_UPDATE_FAILED = "40004"
 ERROR_400_CLASS_NOTICE_DELETE_FAILED = "40005"
 ERROR_400_USER_CREATION_FAILED = "40006"
+"""
 
 
 class BaseAPIException(Exception):
@@ -13,9 +17,20 @@ class BaseAPIException(Exception):
         self.message = message
 
 
+class BaseAuthException(Exception):
+    def __init__(self, code: str, message: str):
+        self.code = code
+        self.message = message
+
+
 class MovieNotFoundException(BaseAPIException):
     def __init__(self):
         super().__init__(code=ERROR_400_MOVIE_NOT_FOUND, message="Class not found")
+
+
+class InvalidAPIKey(BaseAuthException):
+    def __init__(self):
+        super().__init__(code=ERROR_401_INVALID_API_KEY, messeage="Invalid API Key")
 
 
 # class ClassCreationFailed(BaseAPIException):
--- app/routers/__init__.py
+++ app/routers/__init__.py
@@ -1,7 +1,8 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 
 from app.routers import movie
+from app.core.auth import validate_api_key
 
-router = APIRouter(prefix="/v1")
+router = APIRouter(prefix="/v1", dependencies=[Depends(validate_api_key)])
 
 router.include_router(movie.router, prefix="/movie", tags=["movie"])
