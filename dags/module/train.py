2d6eceb93a66620fdb10acffcad817d9250deb45
kimjae-1 <kimjae1@g.skku.edu>
2024-05-30 05:42:20 +0000

    feat- ColumnTransformer 및 sklearn.pipeline 추가
     #23

--- dags/module/train.py
+++ dags/module/train.py
@@ -1,52 +1,45 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from xgboost import XGBRegressor
-from airflow.providers.postgres.hooks.postgres import PostgresHook
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.metrics import mean_squared_error
 
 import optuna
 from optuna.storages import RDBStorage
-import numpy as np
-from sklearn.model_selection import KFold
-from xgboost import XGBRegressor
-from sklearn.preprocessing import StandardScaler
-from sklearn.metrics import mean_squared_error
-from sklearn.pipeline import Pipeline
-from sklearn.metrics import f1_score
 
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+
+import mlflow
 from module.load_data import load
 from module.preprocess_data import preprocess
 
-import mlflow
-from mlflow.tracking import MlflowClient
-from mlflow.store.artifact.runs_artifact_repo import RunsArtifactRepository
-
-import datetime
-from datetime import timedelta
-import os
-
-
 def train_fn(experiment_name: str, **context):
     mlflow.set_experiment(experiment_name)
-    _data = load()
-    data = preprocess(_data)
+    data = load()
+    data = preprocess(data)
+
     y = data["total"]
     X = data.drop(columns=["total"])
 
-    def manipulate(x_train, x_valid):
-        tmp_x_train = x_train.copy()
-        tmp_x_valid = x_valid.copy()
+    x_train, x_valid, y_train, y_valid = train_test_split(
+        X, y, test_size=0.3, shuffle=True)
+    x_train = x_train.reset_index(drop=True)
+    x_valid = x_valid.reset_index(drop=True)
 
-        tmp_x_train = tmp_x_train.reset_index(drop=True)
-        tmp_x_valid = tmp_x_valid.reset_index(drop=True)
+    cat_columns = ['repgenrenm']
+    num_columns = [c for c in X.columns if c not in cat_columns]
 
-        num_columns = tmp_x_train.columns
+    preprocessor = ColumnTransformer(
+        transformers=[
+            ("scaler", StandardScaler(), num_columns),
+            ("encoding", OneHotEncoder(handle_unknown="ignore", sparse_output=False), cat_columns)
+        ]
+    )
 
-        # scaling
-        scaler = StandardScaler()
-        tmp_x_train[num_columns] = scaler.fit_transform(tmp_x_train[num_columns])
-        tmp_x_valid[num_columns] = scaler.transform(tmp_x_valid[num_columns])
-
-        return tmp_x_train, tmp_x_valid
+    x_train = preprocessor.fit_transform(x_train)
+    x_valid = preprocessor.transform(x_valid)
 
     def objective(trial):
         params = {
@@ -61,65 +54,50 @@ def train_fn(experiment_name: str, **context):
             "objective": "reg:squarederror",
             "random_state": 42,
         }
-        rmses = []
-        kf = KFold(n_splits=3, shuffle=True, random_state=42)
+        model = XGBRegressor(**params)
+        model.fit(
+            x_train,
+            y_train,
+            eval_set=[(x_train, y_train), (x_valid, y_valid)],
+            eval_metric="rmse",
+            early_stopping_rounds=20,
+            verbose=False,
+        )
 
-        for i, (trn_idx, val_idx) in enumerate(kf.split(data, y)):
-            x_train, y_train = data.iloc[trn_idx, :], y.iloc[trn_idx]
-            x_valid, y_valid = data.iloc[val_idx, :], y.iloc[val_idx]
+        preds = model.predict(x_valid)
+        rmse = mean_squared_error(y_valid, preds, squared=False)
 
-            # 전처리
-            x_train, x_valid = manipulate(x_train, x_valid)
-            model = XGBRegressor(**params)
-            model.fit(
-                x_train,
-                y_train,
-                eval_set=[(x_train, y_train), (x_valid, y_valid)],
-                eval_metric="rmse",
-                early_stopping_rounds=20,
-                verbose=False,
-            )
+        return rmse
 
-            preds = model.predict(x_valid)
-            rmse = mean_squared_error(y_valid, preds, squared=False)
-            rmses.append(rmse)
-
-        return np.mean(rmses)
-
-    # optuna 저장
     hook = PostgresHook(postgres_conn_id="postgres_default")
     conn = hook.get_conn()
     storage = RDBStorage(url=hook.get_uri().replace("/raw", "/optuna"))
-    # optuna
+
+    # Optuna study
     study = optuna.create_study(
         study_name="movies", direction="minimize", storage=storage, load_if_exists=True
     )
-
-    study.optimize(objective, n_trials=3)
+    study.optimize(objective, n_trials=10)
     best_params = study.best_params
 
     model = XGBRegressor(**best_params)
-    x_train, x_valid, y_train, y_valid = train_test_split(
-        X, y, test_size=0.3, random_state=42
-    )
-    x_train_scaled, x_valid_scaled = manipulate(x_train, x_valid)
-    model.fit(x_train_scaled, y_train)
+    model.fit(x_train, y_train)
 
-    # 메트릭 계산
-    preds = model.predict(x_valid_scaled)
-    rmse = mean_squared_error(y_valid, preds, squared=False)
-    metrics = {"rmse": rmse}
+    # Model Log
+    pipeline = Pipeline([("preprocessor", preprocessor), ("model", model)])
+
+    metrics = {
+        "mse": mean_squared_error(y_valid, model.predict(x_valid), squared=False)
+    }
 
     with mlflow.start_run():
         mlflow.log_params(best_params)
         mlflow.log_metrics(metrics)
-        model_info = mlflow.sklearn.log_model(model, "model")
+        model_info = mlflow.sklearn.log_model(pipeline, "model")
 
     context["ti"].xcom_push(key="run_id", value=model_info.run_id)
     context["ti"].xcom_push(key="model_uri", value=model_info.model_uri)
-    context["ti"].xcom_push(key="eval_metric", value="rmse")
+    context["ti"].xcom_push(key="eval_metric", value="mse")
     print(
         f"Done Train model, run_id: {model_info.run_id}, model_uri: {model_info.model_uri}"
     )
-
-    return best_params
