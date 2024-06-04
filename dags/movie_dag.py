7a82b054ef0b4d582108c2c50c901920b89d6301
SuminHam <hsm4317@gmail.com>
2024-06-04 10:23:20 +0000

    feat - add dependency between dags

--- dags/movie_dag.py
+++ dags/movie_dag.py
@@ -1,6 +1,8 @@
 from airflow.decorators import dag, task
 from airflow.operators.python import PythonOperator
 from airflow.operators.empty import EmptyOperator
+from airflow.sensors.external_task import ExternalTaskSensor
+
 from pendulum import datetime
 
 from module import train
@@ -17,6 +19,16 @@ from module.transit_model import transition_model_stage
 )
 def movie_pipeline():
     start_task = EmptyOperator(task_id="start_task")
+
+    # ExternalTaskSensor to wait for data_pipeline DAG to complete
+    wait_for_data_pipeline = ExternalTaskSensor(
+        task_id="wait_for_data_pipeline",
+        external_dag_id="data_pipeline",
+        external_task_id="end_task",
+        allowed_states=["success"],
+        failed_states=["failed", "skipped"],
+        mode="reschedule",
+    )
 
     train_task = PythonOperator(
         task_id="train_task",
@@ -36,7 +48,14 @@ def movie_pipeline():
 
     end_task = EmptyOperator(task_id="end_task")
 
-    start_task >> train_task >> model_create_task >> model_transition_task >> end_task
+    (
+        start_task
+        >> wait_for_data_pipeline
+        >> train_task
+        >> model_create_task
+        >> model_transition_task
+        >> end_task
+    )
 
 
 movie_pipeline()
