18419a809cd67b3be6fc6c94dfb754a7ce52d9ec
SuminHam <hsm4317@gmail.com>
2024-06-14 19:43:46 +0000

    feat - predict input 전처리 utils에 추가
    #41

--- app/utils.py
+++ app/utils.py
@@ -1,6 +1,8 @@
 import os
 import mlflow
+import pandas as pd
 from app.core.config import config
+from app.core.errors import error
 
 
 def load_model():
@@ -14,3 +16,27 @@ def load_model():
     model_stage = config.MODEL_STAGE
     model = mlflow.sklearn.load_model(f"models:/{model_name}/{model_stage}")
     return model
+
+
+def prepare_for_predict(movie_data):
+    if not movie_data:
+        raise error.MovieNotFoundException()
+
+    if len(movie_data) != 7:
+        raise error.MovieNotEnoughException()
+
+    audiacc = max(row.audiacc for row in movie_data)
+    showacc = sum(row.showcnt for row in movie_data)
+    scrnacc = sum(row.scrncnt for row in movie_data)
+    repgenrenm = movie_data[0].repgenrenm
+
+    df = pd.DataFrame(
+        {
+            "audiacc": [audiacc],
+            "showAcc": [showacc],
+            "scrnAcc": [scrnacc],
+            "repgenrenm": [repgenrenm],
+        }
+    )
+
+    return df
