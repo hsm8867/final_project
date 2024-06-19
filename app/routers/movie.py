a21b42eb786520be1394421b630d9017e4bae3a2
SuminHam <hsm4317@gmail.com>
2024-06-19 06:40:42 +0000

    feat - dependency injector 추가
    #44

--- app/routers/movie.py
+++ app/routers/movie.py
@@ -1,12 +1,13 @@
-from fastapi import APIRouter, Query
+from fastapi import APIRouter, Query, Depends
+from dependency_injector.wiring import Provide, inject
 
 from app.models.schemas.common import BaseResponse, HttpResponse, ErrorResponse
 from app.models.schemas.movie_ import (
     MovieListResp,
 )
 
-from app.services.movie_service import MovieService
-from app import repositories
+from app.services import MovieService
+from app.core.container import Container
 
 from datetime import datetime
 
@@ -18,12 +19,13 @@ router = APIRouter()
     response_model=BaseResponse,
     responses={400: {"model": ErrorResponse}},
 )
+@inject
 async def showing_movies(
     date: datetime,
     page: int = Query(1, ge=1, description="Page number"),
-    limit: int = Query(10, ge=1, le=100, dexcription="Number of items per page"),
+    limit: int = Query(10, ge=1, le=100, description="Number of items per page"),
+    movie_service: MovieService = Depends(Provide[Container.movie_service]),
 ) -> BaseResponse[MovieListResp]:
-    movie_service = MovieService(repositories.MovieRepository())
-    result = await movie_service.read_showing_movies(date, page, limit)
 
+    result = await movie_service.read_showing_movies(date, page, limit)
     return HttpResponse(content=MovieListResp.from_dto(result))
