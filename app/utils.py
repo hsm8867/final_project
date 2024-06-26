f00722cd5e973ccd037de2b75ecaed07573b1731
SuminHam <hsm4317@gmail.com>
2024-06-26 05:18:26 +0000

    fix - simple fix
    #53

--- app/utils.py
+++ app/utils.py
@@ -6,11 +6,6 @@ from app.core.errors import error
 
 
 def load_model():
-
-    # os.environ["AWS_ACCESS_KEY_ID"] = config.AWS_ACCESS_KEY_ID
-    # os.environ["AWS_SECRET_ACCESS_KEY"] = config.AWS_SECRET_ACCESS_KEY
-    # os.environ["MLFLOW_S3_ENDPOINT_URL"] = config.MLFLOW_S3_ENDPOINT_URL
-    # mlflow.set_tracking_uri("http://10.178.0.3:5000")
     model_name = config.MODEL_NAME
     model_stage = config.MODEL_STAGE
     print("Loading model")
